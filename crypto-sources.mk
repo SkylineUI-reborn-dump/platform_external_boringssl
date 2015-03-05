cf5a701292eba07ea7e30eaed7c5b25637f3880e
Kenny Root <kroot@google.com>
2015-03-04 16:00:41 -0800

    Android: disable assembly for Mac and Windows
    There are various problems with assembly on both the Mac and Windows
    builds, so just disable it for now.
    
    Change-Id: I6163e97089c17f70ef9361279319badd22c354b2

--- crypto-sources.mk
+++ crypto-sources.mk
@@ -2,8 +2,10 @@ LOCAL_ADDITIONAL_DEPENDENCIES += $(LOCAL_PATH)/sources.mk
 include $(LOCAL_PATH)/sources.mk
 
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/src/include -I$(LOCAL_PATH)/src/crypto -Wno-unused-parameter
+ifeq ($(HOST_OS),linux)
 LOCAL_SRC_FILES_x86 = $(linux_x86_sources)
 LOCAL_SRC_FILES_x86_64 = $(linux_x86_64_sources)
+endif
 LOCAL_SRC_FILES_arm = $(linux_arm_sources)
 LOCAL_SRC_FILES_arm64 = $(linux_aarch64_sources)
 LOCAL_SRC_FILES += $(crypto_sources)
