c15ce19cc1ff102ec82cb5784a11e85bd803c0a2
Matthew Maurer <mmaurer@google.com>
2019-04-09 16:44:08 -0700

    Suppress some noisy UBSan sanitizers
    Trusty is using a large set of sanitizers in the test build, but
    boringssl is not clean against some of the noisier ones.
    
    This suppresses those sanitizers locally when they have been turned on
    globally.
    
    Bug: 129300035
    Change-Id: I0d76d150ba682d395d2078adb67b3b418ab4d623

--- rules.mk
+++ rules.mk
@@ -56,6 +56,9 @@ LOCAL_SRC_FILES := $(filter-out src/crypto/x509/by_dir.c,$(LOCAL_SRC_FILES))
 # no-op threading functions.
 MODULE_CFLAGS += -DTRUSTY
 
+# Suppress some UBSan warnings in Trusty build
+MODULE_CFLAGS += -fno-sanitize=unsigned-integer-overflow -fno-sanitize=implicit-integer-sign-change
+
 # Define static armcap based on lk build variables
 MODULE_STATIC_ARMCAP := -DOPENSSL_STATIC_ARMCAP
 toarmcap = $(if $(filter-out 0 false,$(2)),-DOPENSSL_STATIC_ARMCAP_$(1),)
