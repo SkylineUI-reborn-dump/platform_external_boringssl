99537bc70b27036ba54b5ce3e4b6179a0da2d807
Pete Bentley <prb@google.com>
2020-01-10 11:30:14 +0000

    Support hashed directories of root certificated in bssl.
    DO NOT SUBMIT: Submit upstream to BoringSSL.
    
    Test: N/A
    Change-Id: I21bbad3465eec36048213c5aacf9e23efdb8d728

--- src/tool/client.cc
+++ src/tool/client.cc
@@ -121,6 +121,11 @@ static const struct argument kArguments[] = {
         "verification is required.",
     },
     {
+        "-root-cert-dir", kOptionalArgument,
+        "A directory containing one of more root certificate PEM files. "
+        "Implies that verification is required.",
+    },
+    {
         "-early-data", kOptionalArgument, "Enable early data. The argument to "
         "this flag is the early data to send or if it starts with '@', the "
         "file to read from for early data.",
@@ -493,6 +498,16 @@ bool Client(const std::vector<std::string> &args) {
   if (args_map.count("-root-certs") != 0) {
     if (!SSL_CTX_load_verify_locations(
             ctx.get(), args_map["-root-certs"].c_str(), nullptr)) {
+      fprintf(stderr, "Failed to load root certificates.\n");
+      ERR_print_errors_fp(stderr);
+      return false;
+    }
+    SSL_CTX_set_verify(ctx.get(), SSL_VERIFY_PEER, nullptr);
+  }
+
+  if (args_map.count("-root-cert-dir") != 0) {
+    if (!SSL_CTX_load_verify_locations(
+            ctx.get(), nullptr, args_map["-root-cert-dir"].c_str())) {
       fprintf(stderr, "Failed to load root certificates.\n");
       ERR_print_errors_fp(stderr);
       return false;
