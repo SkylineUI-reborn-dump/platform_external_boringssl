a2b4418e18a8132e85bb8a618a79412ae58e3bfe
Wenhao Wang <wenhaowang@google.com>
2020-03-10 14:08:48 -0700

    Adapt boringssl makefile for trusted kernel
    The boringssl is only used by user space before.
    We adjust it's makefile so that kernel space can
    use it as well. More specifically, the CSPRNG of
    trusted kernel needs to call functions of rand.c
    in boringssl.
    
    Bug: 149958771
    Change-Id: I3df4ef3c841be4bca3fe287d5034f19b3c9018ce

--- rules.mk
+++ rules.mk
@@ -53,7 +53,13 @@ LOCAL_SRC_FILES := $(filter-out src/crypto/x509/by_dir.c,$(LOCAL_SRC_FILES))
 
 # BoringSSL detects Trusty based on this define and does things like switch to
 # no-op threading functions.
+# For Trusty kernel compilation, we disable ASM.
+ifeq ($(XBIN_TYPE),USER_TASK)
 MODULE_CFLAGS += -DTRUSTY
+else
+MODULE_CFLAGS += -DTRUSTY -DOPENSSL_NO_ASM
+MODULE_ASMFLAGS += -DTRUSTY -DOPENSSL_NO_ASM
+endif
 
 # Define static armcap based on lk build variables
 MODULE_STATIC_ARMCAP := -DOPENSSL_STATIC_ARMCAP
@@ -76,7 +82,14 @@ GLOBAL_INCLUDES += $(addprefix $(LOCAL_DIR)/,$(LOCAL_C_INCLUDES))
 # scopers. Suppress those APIs.
 GLOBAL_CPPFLAGS += -DBORINGSSL_NO_CXX
 
+ifeq ($(XBIN_TYPE),USER_TASK)
 MODULE_DEPS := \
 	lib/openssl-stubs \
+
+else
+MODULE_DEPS := \
+	trusty/kernel/lib/openssl-stubs \
+
+endif
 
 include make/module.mk
