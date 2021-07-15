2302bd65d55d9705870ce3ebec5e4b9421dd8c38
Stephen Crane <cranes@google.com>
2021-07-15 13:25:26 -0700

    DO NOT MERGE - Always have CYRPTO_sysrand_for_seed.
    Cherry-pick of
    https://boringssl-review.googlesource.com/c/boringssl/+/48485 for
    testing with Trusty.
    
    Test: build.py generic-arm64
    Bug: 193673777
    Change-Id: I5e4e302b6c57fc5e422d590f91e74caf3f20ff13

--- src/crypto/fipsmodule/rand/internal.h
+++ src/crypto/fipsmodule/rand/internal.h
@@ -68,15 +68,15 @@ void RAND_need_entropy(size_t bytes_needed);
 // system.
 void CRYPTO_sysrand(uint8_t *buf, size_t len);
 
-#if defined(OPENSSL_URANDOM)
-// CRYPTO_init_sysrand initializes long-lived resources needed to draw entropy
-// from the operating system.
-void CRYPTO_init_sysrand(void);
-
 // CRYPTO_sysrand_for_seed fills |len| bytes at |buf| with entropy from the
 // operating system. It may draw from the |GRND_RANDOM| pool on Android,
 // depending on the vendor's configuration.
 void CRYPTO_sysrand_for_seed(uint8_t *buf, size_t len);
+
+#if defined(OPENSSL_URANDOM)
+// CRYPTO_init_sysrand initializes long-lived resources needed to draw entropy
+// from the operating system.
+void CRYPTO_init_sysrand(void);
 
 // CRYPTO_sysrand_if_available fills |len| bytes at |buf| with entropy from the
 // operating system, or early /dev/urandom data, and returns 1, _if_ the entropy
@@ -86,10 +86,6 @@ void CRYPTO_sysrand_for_seed(uint8_t *buf, size_t len);
 int CRYPTO_sysrand_if_available(uint8_t *buf, size_t len);
 #else
 OPENSSL_INLINE void CRYPTO_init_sysrand(void) {}
-
-OPENSSL_INLINE void CRYPTO_sysrand_for_seed(uint8_t *buf, size_t len) {
-  CRYPTO_sysrand(buf, len);
-}
 
 OPENSSL_INLINE int CRYPTO_sysrand_if_available(uint8_t *buf, size_t len) {
   CRYPTO_sysrand(buf, len);
