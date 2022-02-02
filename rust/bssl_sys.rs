4551a24fdf001b22d6cdbb2e0d53914c2fc3ba85
David Drysdale <drysdale@google.com>
2022-02-02 17:04:17 +0000

    Build BoringSSL Rust bindings
    Adapt the upstream CMake-based invocation of bindgen for Soong.
    
    Bug: 177080016
    Test: atest bssl_sys_bindgen_test bssl_sys_test
    Change-Id: Iabf1b6ac4ae7d3a391cc038b29f157347fb36f2f

---
+++ rust/bssl_sys.rs
@@ -0,0 +1,36 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+//! Low-level crate providing FFI access to BoringSSL functionality.
+//!
+//! This version is adapted from upstream's rust/src/lib.rs file at:
+//! <https://boringssl.googlesource.com/boringssl/+/refs/heads/master/rust/src/lib.rs>
+//!
+//! Almost all the code in this crate is produced by bindgen running
+//! over the BoringSSL headers. The only manually generated code is
+//! - an `init()` function
+//! - Rust equivalents to functional preprocessor macros defined in the
+//!   BoringSSL headers (as bindgen doesn't cope with them).
+
+use libc::*;
+
+include!(concat!(env!("OUT_DIR"), "/bssl_sys_bindings.rs"));
+
+pub fn ERR_GET_LIB(packed_error: u32) -> i32 {
+    unsafe { ERR_GET_LIB_RUST(packed_error) }
+}
+
+pub fn ERR_GET_REASON(packed_error: u32) -> i32 {
+    unsafe { ERR_GET_REASON_RUST(packed_error) }
+}
+
+pub fn ERR_GET_FUNC(packed_error: u32) -> i32 {
+    unsafe { ERR_GET_FUNC_RUST(packed_error) }
+}
+
+pub fn init() {
+    unsafe {
+        CRYPTO_library_init();
+    }
+}
