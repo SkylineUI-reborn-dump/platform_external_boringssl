588117cc09919d44337716d9a905a372096feccd
Paul Crowley <paulcrowley@google.com>
2022-09-08 22:53:49 +0000

    Ensure clients can't block prng_seeder
    Use async to rework the server so that it can serve new clients while
    old clients are still reading what they've been sent. Add some wiggle room
    on exactly when we reseed the DRBG to support the async case - we
    enforce reseed every 256 requests but we try to reseed every 128. Use
    proper Android logging and always log on exit.
    
    Bug: 243933553
    Test: DO NOT SUBMIT
    Change-Id: Icafa5f27f01c5c453bd8dd1965fb5b6a4ad27dbb

--- prng_seeder/src/main.rs
+++ prng_seeder/src/main.rs
@@ -3,14 +3,25 @@ mod cutils_socket;
 mod drbg;
 
 use std::{
-    io::{ErrorKind, Write},
+    convert::Infallible,
+    fs::remove_file,
+    io::ErrorKind,
     os::unix::net::UnixListener,
-    path::PathBuf,
+    path::{Path, PathBuf},
+    sync::Arc,
 };
 
 use anyhow::Result;
 use clap::Parser;
+use log::{error, info};
 use nix::sys::signal;
+use tokio::{
+    io::AsyncWriteExt,
+    net::{UnixListener as TokioUnixListener, UnixStream},
+    sync::Mutex,
+};
+
+use crate::conditioner::Conditioner;
 
 #[derive(Debug, clap::Parser)]
 struct Cli {
@@ -20,30 +31,73 @@ struct Cli {
     socket: Option<PathBuf>,
 }
 
-fn main() -> Result<()> {
-    let cli = Cli::parse();
-    println!("{:?}", cli);
+fn configure_logging() {
+    logger::init(Default::default());
+}
+
+fn get_socket(path: &Path) -> Result<UnixListener> {
+    if let Err(e) = remove_file(path) {
+        if e.kind() != ErrorKind::NotFound {
+            return Err(e.into());
+        }
+    } else {
+        info!("Deleted old {}", path.to_string_lossy());
+    }
+    Ok(UnixListener::bind(path)?)
+}
+
+async fn handle_stream(conditioner: Arc<Mutex<Conditioner>>, mut stream: UnixStream) -> Result<()> {
+    let bytes = conditioner.lock().await.request()?;
+    stream.write_all(&bytes).await?;
+    conditioner.lock().await.reseed_if_necessary().await?;
+    Ok(())
+}
+
+async fn listen_loop(conditioner: Conditioner, listener: TokioUnixListener) -> Result<Infallible> {
+    let conditioner = Arc::new(Mutex::new(conditioner));
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                let conditioner = conditioner.clone();
+                tokio::spawn(async move {
+                    if let Err(e) = handle_stream(conditioner, stream).await {
+                        error!("Request failed: {}", e);
+                    }
+                });
+            }
+            Err(e) if e.kind() == ErrorKind::Interrupted => {}
+            Err(e) => return Err(e.into()),
+        }
+    }
+}
+
+fn run(cli: Cli) -> Result<Infallible> {
     let hwrng = std::fs::File::open(&cli.source)?;
     let listener = match cli.socket {
-        Some(path) => UnixListener::bind(&path)?,
+        Some(path) => get_socket(path.as_path())?,
         None => cutils_socket::android_get_control_socket("prng_seeder")?,
     };
 
     unsafe { signal::signal(signal::Signal::SIGPIPE, signal::SigHandler::SigIgn) }?;
 
-    let mut conditioner = conditioner::Conditioner::new(hwrng)?;
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let conditioner = Conditioner::new(hwrng)?;
+            let listener = TokioUnixListener::from_std(listener)?;
+            listen_loop(conditioner, listener).await
+        })
+}
 
-    for mut stream in listener.incoming() {
-        match stream {
-            Ok(ref mut stream) => {
-                stream.write_all(&conditioner.request()?)?;
-                conditioner.reseed_if_necessary()?;
-            }
-            Err(e) if e.kind() == ErrorKind::Interrupted => {}
-            e => {
-                e?;
-            }
-        }
+fn main() {
+    let cli = Cli::parse();
+    configure_logging();
+    if let Err(e) = run(cli) {
+        error!("Launch failed: {}", e);
+    } else {
+        error!("Loop terminated without an error")
     }
-    Ok(())
+    std::process::exit(-1);
 }
